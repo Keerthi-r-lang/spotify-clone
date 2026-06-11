75f3395c0a0a4800aa015842d4469033b128b79d
Keerthi-r-lang <keerthiburle@gmail.com>
2026-06-11 22:25:36 +0530

    Fix empty CORS origin issue

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    os.environ.get('FRONTEND_URL', ''),
-]
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
+frontend_url = os.environ.get('FRONTEND_URL')
+if frontend_url:
+    CORS_ALLOWED_ORIGINS.append(frontend_url)
 # JWT
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
