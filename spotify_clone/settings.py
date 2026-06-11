b130d4ae344f289e52c32734cdfea2d27614c86c
Keerthi-r-lang <keerthiburle@gmail.com>
2026-06-11 22:39:26 +0530

    Fix ALLOWED_HOSTS for Render

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -34,8 +34,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,spotify-clone-api-fwcy.onrender.com'
+).split(',')
 
 
 # Application definition
